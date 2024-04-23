13bb69c258216b08247405704d484c432c4bd420
Aleksey <11_alx_11@ro.ru>
2024-04-23 12:01:20 +0300

    init labrab7

---
+++ labrab7/src/com/labrab7/task2/Main.java
@@ -0,0 +1,15 @@
+package com.labrab7.task2;
+
+/**
+ * Вариант №2: <strong>Н-фрактал</strong> <hr>
+ * Реализовать построение заданного типа фрактала по варианту.
+ * Везде, где это необходимо, предусмотреть ввод параметров, влияющих на внешний вид фрактала.
+ * Н-фрактал
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
