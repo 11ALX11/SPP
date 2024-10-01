332354a85c93b0119d91ede8bf1bae504293123f
Aleksey <11_alx_11@ro.ru>
2024-10-01 09:10:37 +0300

    init project

---
+++ s2labrab1/src/main/java/SPP/s2labrab1/Main.java
@@ -0,0 +1,10 @@
+package SPP.s2labrab1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Task1.main(args);
+        Task2.main(args);
+    }
+}
---
+++ s2labrab1/src/main/java/SPP/s2labrab1/Task1.java
@@ -0,0 +1,14 @@
+package SPP.s2labrab1;
+
+/**
+ * Вариант 2 <br>
+ * Напишите программу, которая вводит строку текста,
+ * разбивает ее на лексемы выводит лексемы в обратном порядке.
+ */
+public class Task1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
