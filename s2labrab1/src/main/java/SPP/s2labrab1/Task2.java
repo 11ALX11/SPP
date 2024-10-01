c2a4a66edc74c94ee65ba67617d1d13b993eff57
Aleksey <11_alx_11@ro.ru>
2024-10-01 10:54:15 +0300

    working on Task2.java

--- s2labrab1/src/main/java/SPP/s2labrab1/Task2.java
+++ s2labrab1/src/main/java/SPP/s2labrab1/Task2.java
@@ -1,5 +1,9 @@
 package SPP.s2labrab1;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  * Вариант 2 — tail
  * <br>
@@ -33,6 +37,87 @@ public class Task2
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int linesToRead = 0;
+        String filename = null;
+
+        boolean expectNumber = false;
+        for (String arg : args)
+        {
+            if (arg.startsWith("-") || expectNumber)
+            {
+                String number = arg.substring(1);
+                if (arg.startsWith("-n"))
+                {
+                    number = arg.substring(2);
+                    if (number.equals(""))
+                    {
+                        expectNumber = true;
+                        continue;
+                    }
+                }
+                if (expectNumber)
+                {
+                    number = arg;
+                    expectNumber = false;
+                }
+
+                try
+                {
+                    linesToRead = Integer.parseInt(number);
+                } catch (NumberFormatException e)
+                {
+                    System.err.println("Неверный формат числа: " + number);
+                    return;
+                }
+            } else
+            {
+                filename = arg;
+            }
+        }
+
+        if (filename == null)
+        {
+            System.err.println("Не указан файл.");
+            return;
+        }
+        if (linesToRead <= 0)
+        {
+            linesToRead = 10;
+        }
+
+
+        try (RandomAccessFile file = new RandomAccessFile(filename, "r"))
+        {
+            long pointer = file.length() - 1;
+            int lineCount = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while (pointer >= 0 && lineCount < linesToRead)
+            {
+                file.seek(pointer);
+                char currentChar = (char) file.readByte();
+
+                if (currentChar == '\n')
+                {
+                    lineCount++;
+                    if (lineCount == linesToRead)
+                    {
+                        break;
+                    }
+                }
+
+                sb.append(currentChar);
+                pointer--;
+            }
+
+            System.out.print(sb.reverse());
+
+        } catch (FileNotFoundException e)
+        {
+            System.err.println("404: Файл не найден.");
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
