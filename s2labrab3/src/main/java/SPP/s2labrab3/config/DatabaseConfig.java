67f34f39b2235a274cfd498e75de95b2af83d627
Aleksey <11_alx_11@ro.ru>
2024-12-24 10:30:16 +0300

    working on s2labrab3

--- s2labrab3/src/main/java/SPP/s2labrab3/config/DatabaseConfig.java
+++ s2labrab3/src/main/java/SPP/s2labrab3/config/DatabaseConfig.java
@@ -1,7 +1,9 @@
 package SPP.s2labrab3.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig
@@ -10,8 +12,24 @@ public class DatabaseConfig
     private static final String USER = "";
     private static final String PASSWORD = "";
 
-    public static Connection getConnection() throws SQLException
-    {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl(URL);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        ds = new HikariDataSource(config);
     }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    private DatabaseConfig(){}
 }
